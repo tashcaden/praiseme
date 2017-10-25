ec4c7f23a18922929eb8ef5d11617bdec9372430
Natasha Cadenhead <ncadenhead@unimelb.edu.au>
2017-10-25 15:20:35 +1100

    Added an example to praise function to help users

--- R/praise.R
+++ R/praise.R
@@ -3,6 +3,12 @@
 #' @description Praise me at a critical time in my life
 #'
 #' @export
+#'
+#' @examples
+#'
+#' #I'm gonna deliver some praise!
+#' praise()
+#'
 praise <- function() {
   print("You're the best")
 }
